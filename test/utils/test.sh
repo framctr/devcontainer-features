70e70ee0ff6c6d031917c1236d784fd63db76705
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-10-04 13:52:08 +0000

    Update also the test script for utils

--- test/utils/test.sh
+++ test/utils/test.sh
@@ -9,8 +9,7 @@ set -e
 # Provides the 'check' and 'reportResults' commands.
 source dev-container-features-test-lib
 
-check "execute command" bash -c "man --version"
-check "execute command" bash -c "ping --version"
+check "execute command" bash -c "ping -V"
 check "execute command" bash -c "ipcalc --version"
 check "execute command" bash -c "curl --version"
 check "execute command" bash -c "wget --version"
