ff4fab995a625470063c6c84230edc92fd3404cb
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-10-04 08:19:17 +0000

    Move to nanolayer pipx installation

--- src/openstack-client/install.sh
+++ src/openstack-client/install.sh
@@ -2,37 +2,22 @@
 set -e
 
 echo "Activating feature 'openstack-client'"
-OPENSTACK_CLIENT_VERSION="${VERSION}"
 
-# Clean up
-rm -rf /var/lib/apt/lists/*
+set -e
 
-if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-    exit 1
-fi
+. ./library_scripts.sh
 
-require_pip() {
-    if [ -z "$(which pip)" ] ; then
-        apt-get update && apt-get install -y pip
-    fi
-}
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.0"
 
-# The 'install.sh' entrypoint script is always executed as the root user.
-#
-# These following environment variables are passed in by the dev container CLI.
-# These may be useful in instances where the context of the final 
-# remoteUser or containerUser is useful.
-# For more details, see https://containers.dev/implementors/features#user-env-var
-# echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-# echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/pipx-package:1.1.7" \
+    --option package='python-openstackclient' --option version="$VERSION"
 
-# echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-# echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-
-require_pip
-if [ "${OPENSTACK_CLIENT_VERSION}" != "latest" ] ; then
-    pip install python-openstackclient=="${OPENSTACK_CLIENT_VERSION}"
-else
-    pip install python-openstackclient
-fi
+echo 'Done!'
