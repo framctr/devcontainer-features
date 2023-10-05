7bb348e572ddd67c2c1574b9e516297cf38f5a3c
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-10-05 06:58:54 +0000

    Add more openstack clients

--- src/openstack-client/install.sh
+++ src/openstack-client/install.sh
@@ -11,10 +11,11 @@ set -e
 ensure_nanolayer nanolayer_location "v0.5.0"
 
 install_package() {
-    local package_name="$1"
+    local package_name="$1" | tr '[:upper:]' '[:lower:]' # convert to lowercase
     local package_version="$2"
 
     $nanolayer_location install apt-get pip,python3-dev,gcc
+
     if [ "$package_version" != "latest" ] ; then
         pip install "${package_name}"=="${package_version}"
     else
@@ -25,9 +26,81 @@ install_package() {
 # Install primary client
 install_package python-openstackclient "$VERSION"
 
-# Install other packages
+# Install other clients
+if [ "${BARBICAN}" != "none" ] ; then
+    install_package "${!BARBICAN@}" "$BARBICAN" # install the package passing the variable name and value
+fi
+
+if [ "${CEILOMETER}" != "none" ] ; then
+    install_package "${!CEILOMETER@}" "$CEILOMETER" # install the package passing the variable name and value
+fi
+
+if [ "${CINDER}" != "none" ] ; then
+    install_package "${!CINDER@}" "$CINDER" # install the package passing the variable name and value
+fi
+
+if [ "${CLOUDKITTY}" != "none" ] ; then
+    install_package "${!CLOUDKITTY@}" "$CLOUDKITTY" # install the package passing the variable name and value
+fi
+
+if [ "${DESIGNATE}" != "none" ] ; then
+    install_package "${!DESIGNATE@}" "$DESIGNATE" # install the package passing the variable name and value
+fi
+
+if [ "${FUEL}" != "none" ] ; then
+    install_package "${!FUEL@}" "$FUEL" # install the package passing the variable name and value
+fi
+
+if [ "${GLANCE}" != "none" ] ; then
+    install_package "${!GLANCE@}" "$GLANCE" # install the package passing the variable name and value
+fi
+
+if [ "${GNOCCHI}" != "none" ] ; then
+    install_package "${!GNOCCHI@}" "$GNOCCHI" # install the package passing the variable name and value
+fi
+
+if [ "${HEAT}" != "none" ] ; then
+    install_package "${!HEAT@}" "$HEAT" # install the package passing the variable name and value
+fi
+
+if [ "${MAGNUM}" != "none" ] ; then
+    install_package "${!MAGNUM@}" "$MAGNUM" # install the package passing the variable name and value
+fi
+
+if [ "${MANILA}" != "none" ] ; then
+    install_package "${!MANILA@}" "$MANILA" # install the package passing the variable name and value
+fi
+
+if [ "${MISTRAL}" != "none" ] ; then
+    install_package "${!MISTRAL@}" "$MISTRAL" # install the package passing the variable name and value
+fi
+
+if [ "${MONASCA}" != "none" ] ; then
+    install_package "${!MONASCA@}" "$MONASCA" # install the package passing the variable name and value
+fi
+
+if [ "${MURANO}" != "none" ] ; then
+    install_package "${!MURANO@}" "$MURANO" # install the package passing the variable name and value
+fi
+
 if [ "${OCTAVIA}" != "none" ] ; then
-    install_package python-octaviaclient "$OCTAVIA"
+    install_package "${!OCTAVIA@}" "$OCTAVIA" # install the package passing the variable name and value
+fi
+
+if [ "${SAHARA}" != "none" ] ; then
+    install_package "${!SAHARA@}" "$SAHARA" # install the package passing the variable name and value
+fi
+
+if [ "${SENLIN}" != "none" ] ; then
+    install_package "${!SENLIN@}" "$SENLIN" # install the package passing the variable name and value
+fi
+
+if [ "${SWIFT}" != "none" ] ; then
+    install_package "${!SWIFT@}" "$SWIFT" # install the package passing the variable name and value
+fi
+
+if [ "${TROVE}" != "none" ] ; then
+    install_package "${!TROVE@}" "$TROVE" # install the package passing the variable name and value
 fi
 
 echo 'Done!'
--- test/openstack-client/check_latest_version.sh
+++ test/openstack-client/check_latest_version.sh
@@ -10,7 +10,25 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 check "execute command" bash -c "openstack --version"
+check "execute command" bash -c "pip list installed | grep python-barbicanclient"
+check "execute command" bash -c "pip list installed | grep python-ceilometerclient"
+check "execute command" bash -c "pip list installed | grep python-cinderclient"
+check "execute command" bash -c "pip list installed | grep python-cloudkittyclient"
+check "execute command" bash -c "pip list installed | grep python-designateclient"
+check "execute command" bash -c "pip list installed | grep python-fuelclient"
+check "execute command" bash -c "pip list installed | grep python-glanceclient"
+check "execute command" bash -c "pip list installed | grep python-gnocchiclient"
+check "execute command" bash -c "pip list installed | grep python-heatclient"
+check "execute command" bash -c "pip list installed | grep python-magnumclient"
+check "execute command" bash -c "pip list installed | grep python-manilaclient"
+check "execute command" bash -c "pip list installed | grep python-mistralclient"
+check "execute command" bash -c "pip list installed | grep python-monascaclient"
+check "execute command" bash -c "pip list installed | grep python-muranoclient"
 check "execute command" bash -c "pip list installed | grep python-octaviaclient"
+check "execute command" bash -c "pip list installed | grep python-saharaclient"
+check "execute command" bash -c "pip list installed | grep python-senlinclient"
+check "execute command" bash -c "pip list installed | grep python-swiftclient"
+check "execute command" bash -c "pip list installed | grep python-troveclient"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
