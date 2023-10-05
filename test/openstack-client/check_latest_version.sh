b67b8150d6df205cd124c689dd331cb4aef2ff29
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-10-05 07:53:11 +0000

    Single check openstack tests

--- test/openstack-client/check_latest_version.sh
+++ test/openstack-client/check_latest_version.sh
@@ -9,26 +9,27 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "openstack --version"
-check "execute command" bash -c "pip list installed | grep python-barbicanclient"
-check "execute command" bash -c "pip list installed | grep python-ceilometerclient"
-check "execute command" bash -c "pip list installed | grep python-cinderclient"
-check "execute command" bash -c "pip list installed | grep python-cloudkittyclient"
-check "execute command" bash -c "pip list installed | grep python-designateclient"
-check "execute command" bash -c "pip list installed | grep python-fuelclient"
-check "execute command" bash -c "pip list installed | grep python-glanceclient"
-check "execute command" bash -c "pip list installed | grep python-gnocchiclient"
-check "execute command" bash -c "pip list installed | grep python-heatclient"
-check "execute command" bash -c "pip list installed | grep python-magnumclient"
-check "execute command" bash -c "pip list installed | grep python-manilaclient"
-check "execute command" bash -c "pip list installed | grep python-mistralclient"
-check "execute command" bash -c "pip list installed | grep python-monascaclient"
-check "execute command" bash -c "pip list installed | grep python-muranoclient"
-check "execute command" bash -c "pip list installed | grep python-octaviaclient"
-check "execute command" bash -c "pip list installed | grep python-saharaclient"
-check "execute command" bash -c "pip list installed | grep python-senlinclient"
-check "execute command" bash -c "pip list installed | grep python-swiftclient"
-check "execute command" bash -c "pip list installed | grep python-troveclient"
+check "execute command" bash -c \
+    "openstack --version \
+  && pip list installed | grep python-barbicanclient \
+  && pip list installed | grep python-ceilometerclient \
+  && pip list installed | grep python-cinderclient \
+  && pip list installed | grep python-cloudkittyclient \
+  && pip list installed | grep python-designateclient \
+  && pip list installed | grep python-fuelclient \
+  && pip list installed | grep python-glanceclient \
+  &6 pip list installed | grep python-gnocchiclient \
+  && pip list installed | grep python-heatclient \
+  && pip list installed | grep python-magnumclient \
+  && pip list installed | grep python-manilaclient \
+  && pip list installed | grep python-mistralclient \
+  && pip list installed | grep python-monascaclient \
+  && pip list installed | grep python-muranoclient \
+  && pip list installed | grep python-octaviaclient \
+  && pip list installed | grep python-saharaclient \
+  && pip list installed | grep python-senlinclient \
+  && pip list installed | grep python-swiftclient \
+  && pip list installed | grep python-troveclient"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
