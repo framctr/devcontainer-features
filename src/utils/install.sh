f6f2ee8af24975c52949031f91f0b4df1eed72b3
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-10-10 12:53:24 +0000

    Add extra apt pkgs in utils & use nanolayer installer

--- src/utils/install.sh
+++ src/utils/install.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+. ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.0"
+
 utils=""
 
 if [ "${PING}" = "true" ] ; then
@@ -27,7 +36,12 @@ if [ "${JQ}" = "true" ] ; then
     utils="${utils} jq"
 fi
 
-apt-get update && apt-get install -y $utils
+if [ -n "${EXTRA_APT_PACKAGES}" ] ; then
+    utils="${utils} ${EXTRA_APT_PACKAGES}"
+fi
+
+install_packages=$(echo "${utils}" | xargs | tr ' ' ',' ) # trim and replace space with comma
+$nanolayer_location install apt-get "${install_packages}"
 
 if [ "${YQ}" = "true" ] ; then
     wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
