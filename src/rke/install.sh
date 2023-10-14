d3d6652e2a84fa1f20d14187d627d8434a9ac768
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-10-14 06:45:29 +0000

    Fix version rke

--- src/rke/install.sh
+++ src/rke/install.sh
@@ -4,7 +4,7 @@ set -e
 local_bin_dir="/usr/local/bin"
 
 if [ "${VERSION}" = "latest" ] ; then
-   VERSION=curl -s https://api.github.com/repos/rancher/rke/releases/latest | grep "tag_name" | cut -d : -f 2,3 | tr -d \" | tr -d , | tr -d ' '
+   VERSION=$(curl -s https://api.github.com/repos/rancher/rke/releases/latest | grep "tag_name" | cut -d : -f 2,3 | tr -d \" | tr -d , | tr -d ' ')
 fi
 
 wget -O- https://github.com/rancher/rke/releases/download/${VERSION}/rke_linux-amd64 > rke
