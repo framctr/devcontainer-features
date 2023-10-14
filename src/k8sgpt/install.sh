121202ccf92d2e09bd76563a6302b8dda9cd863f
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-10-14 06:49:14 +0000

    Fix version latest in k8sgpt install

--- src/k8sgpt/install.sh
+++ src/k8sgpt/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "${VERSION}" = "latest" ] ; then
-   VERSION=curl -s https://api.github.com/repos/k8sgpt-ai/k8sgpt/releases/latest | grep "tag_name" | cut -d : -f 2,3 | tr -d \" | tr -d , | tr -d ' '
+   VERSION=$(curl -s https://api.github.com/repos/k8sgpt-ai/k8sgpt/releases/latest | grep "tag_name" | cut -d : -f 2,3 | tr -d \" | tr -d , | tr -d ' ')
 fi
 
 curl -LO https://github.com/k8sgpt-ai/k8sgpt/releases/download/${VERSION}/k8sgpt_amd64.deb
