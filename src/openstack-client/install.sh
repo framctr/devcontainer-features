6b791007823d1dbf03821b9d642de27fbc84b83a
framctr <35109437+framctr@users.noreply.github.com>
2023-12-22 12:19:32 +0100

    Restore nanolayer in openstack client

--- src/openstack-client/install.sh
+++ src/openstack-client/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+. ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.0"
+
 install_package() {
     local package_name=$(echo "$1" | tr '[:upper:]' '[:lower:]') # convert to lowercase
     local package_version="$2"
@@ -19,7 +28,7 @@ install_package() {
 # ---------------------
 # Installation
 
-apt-get update && apt-get install -y pip python3-dev gcc
+$nanolayer_location install apt-get pip,python3-dev,gcc
 
 # Install primary client
 install_package python-openstack "$VERSION"
