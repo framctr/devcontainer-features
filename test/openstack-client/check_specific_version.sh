f8cf6415e144b7f4a2a3111a7b53fdc95655cfb8
Francesco Maria Cultrera <f.cultrera@cineca.it>
2023-12-22 10:45:50 +0000

    Add test for octavia client

--- test/openstack-client/check_specific_version.sh
+++ test/openstack-client/check_specific_version.sh
@@ -10,6 +10,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 check "openstack --version | grep 6.3.0" openstack --version | grep "6.3.0"
+check "pip list installed | grep -e 'octavia.*3.6.0'" pip list installed | grep -e 'octavia.*3.6.0'
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
