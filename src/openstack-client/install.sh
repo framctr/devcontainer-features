56987ddf5502f6d5e8454ad3519dfeb2bc57db6a
framctr <35109437+framctr@users.noreply.github.com>
2023-12-22 11:33:14 +0100

    Drop nanolayer installation in openstack feature

--- src/openstack-client/install.sh
+++ src/openstack-client/install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-. ./library_scripts.sh
-
-# nanolayer is a cli utility which keeps container layers as small as possible
-# source code: https://github.com/devcontainers-contrib/nanolayer
-# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
-# and if missing - will download a temporary copy that automatically get deleted at the end 
-# of the script
-ensure_nanolayer nanolayer_location "v0.5.0"
-
 install_package() {
     local package_name=$(echo "$1" | tr '[:upper:]' '[:lower:]') # convert to lowercase
     local package_version="$2"
@@ -28,7 +19,7 @@ install_package() {
 # ---------------------
 # Installation
 
-$nanolayer_location install apt-get pip,python3-dev,gcc
+apt-get install -y pip python3-dev gcc
 
 # Install primary client
 install_package python-openstack "$VERSION"
